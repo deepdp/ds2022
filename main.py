3c57b0bb73082b9f9e61b81fd9e2ae83191fd074
Dmytro Ponomarov <dpono@prosave.dk>
2023-02-21 06:42:16 +0100

    Heartbeat and part 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 from fastapi import FastAPI
 from pydantic import BaseModel
-from requests import RequestException
+from requests import *
 import uvicorn
 import os
 import logging
@@ -20,6 +20,7 @@ secondaries = {
     'http://localhost:8001/append',
     'http://localhost:8002/append',
 }
+secondariesStatus = {}
 
 # Message class description
 class MSG(BaseModel):
@@ -34,32 +35,50 @@ app = FastAPI()
 async def get_msgs_list():
     return DSServices.get_msgs(storage)
 
+# Health status
+@app.get('/health')
+async def get_health_status(request: Request):
+    # 
+    # Check response status code by own url.
+    response_code = DSServices.get_healts_code(str(Request.url))
+    health_status = 'Unhealthy'
+    # Active
+    if response_code == 200:
+        health_status = 'Healthy'
+    # Busy    
+    elif response_code == 204:
+        health_status = 'Healthy'
+
+    return health_status
+
 @app.post('/append')
-async def set_msg(msg: MSG):
+async def set_msg(msg: MSG, write_concern: int = 1):
     logger.info(os.getenv('APP_LEVEL'))
     # Master part
-    if os.getenv('APP_LEVEL') == 'master':
-        # Save on master
-        masterResponse = DSServices.post_msg(msg, storage)
-        
+    if os.getenv('APP_LEVEL') == 'master':        
         # Replicate to secondaries
-        allGood = True
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            results = []
-            for url in secondaries:
-                results.append(executor.submit(DSServices.postRequest, url=url, data=msg))
-            for future in concurrent.futures.as_completed(results):
-                try:
-                    logger.info(future.result())
-                except RequestException as e:
-                    allGood = False
-                    logger.info(e)
+        if (write_concern > 1):
+            allGood = True
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                results = []
+                for url in secondaries:
+                    results.append(executor.submit(DSServices.postRequest, url=url, data=msg))
+                for future in concurrent.futures.as_completed(results):
+                    try:
+                        logger.info(future.result())
+                    except RequestException as e:
+                        allGood = False
+                        logger.info(e)
 
-        if not allGood:
-            return 'Something went wrong! Please check logs.'            
+            # All messages should be appended.
+            # Count of appended messages must follw wtite concern rules.
+            if not allGood and ((len(results) - 1) != write_concern):
+                return 'Something went wrong! Please check logs.'        
 
+    # Save on master
+    masterResponse = DSServices.post_msg(msg, storage)
        
-    # Secondary part
+    # Secondaries part
     secondaryResponses = []
     if os.getenv('APP_LEVEL') == 'secondary':
         secondaryResponses.append(DSServices.post_msg(msg, storage))
@@ -71,5 +90,7 @@ async def set_msg(msg: MSG):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('APP_PORT')))
+    # Start heartbeater
+    DSServices.heartbeat_it(secondaries, secondariesStatus)
     # Remove Test 
     #uvicorn.run(app, host="0.0.0.0", port=int(5007))
--- resourses/ds_functions.py
+++ resourses/ds_functions.py
@@ -18,6 +18,11 @@ class DSServices:
 
         return storage
 
+    """
+    msg: Messege
+    storage: array (memory) for a starage
+    w: write convcern (Optional, used in calls to seconderies)
+    """
     def post_msg(msg: str, storage: dict):
         try:
             # Set timeouts
@@ -36,3 +41,33 @@ class DSServices:
     def postRequest(url, data):
         response = requests.post(url, [], timeout=10)
         return response.status_code
+
+
+    def get_write_concern(write_concern: int = None):
+        return write_concern
+
+
+    def get_healts_code(url):
+        try:
+            response = requests.get(url)
+            return response.status_code
+        except:
+            logger.error('Could not get server health status')
+            return 0
+    
+    def set_health_status(secondariesStatus: dict, current_status: str, secondary_url: str):
+        secondariesStatus[secondary_url] = current_status
+
+    def heartbeat_it(self, urls: dict, secondariesStatus: dict):
+        for i in range(10):
+            try:
+                time.sleep(0.5)
+                for url in urls:
+                    DSServices.set_health_status(secondariesStatus, self.get_healts_code(url), url)
+
+                
+                break
+            except Exception:
+                logger.error('Could not get server health status')
+                continue
+
